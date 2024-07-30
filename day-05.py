6420369271e7552af619378a937db7afb3e0fa2c
Chinmay Mathur <167203443+chinmaymathur@users.noreply.github.com>
2024-07-30 22:43:20 +0530

    Add files via upload

---
+++ day-05.py
@@ -0,0 +1,29 @@
+#module
+manager_name = "Kishan solanki"
+manager_id = "kishan@gmail.com"
+manager_pass = "hello543"
+manager_salary = 82000
+
+#employee records
+employee_list = ['rohit','abhishek','harsh'] 
+rohit_id = 'rohit@gmail.com'
+rohit_pass = "rohit989"
+
+
+def manager_data():
+    print(manager_name)
+    print(manager_id)
+
+def employee_data():
+    print("your employee list : ",employee_list)
+    print(manager_name)
+    print(manager_id)
+
+#function ko call kro
+employee_data()
+print("python manager")
+manager_data()
+
+
+
+
